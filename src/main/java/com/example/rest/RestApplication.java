6577baf5f8f1145efc865801fa62fc7a9bc62c4f
Mateusz Kopczan <mkopczan1998@gmail.com>
2022-06-27 23:17:42 +0200

    update RestApplication.java

--- src/main/java/com/example/rest/RestApplication.java
+++ src/main/java/com/example/rest/RestApplication.java
@@ -25,31 +25,30 @@ public class RestApplication {
 
 
     @Bean
-    PasswordEncoder passwordEncoder(){
+    PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
     @Bean
-    CommandLineRunner run(UserService userService){
+    CommandLineRunner run(UserService userService) {
         return args -> {
-          userService.saveRole(new Role(null, "ROLE_USER"));
-          userService.saveRole(new Role(null, "ROLE_MANAGER"));
-          userService.saveRole(new Role(null, "ROLE_ADMIN"));
-          userService.saveRole(new Role(null, "ROLE_SUPER_ADMIN"));
+            userService.saveRole(new Role(null, "ROLE_USER"));
+            userService.saveRole(new Role(null, "ROLE_MANAGER"));
+            userService.saveRole(new Role(null, "ROLE_ADMIN"));
+            userService.saveRole(new Role(null, "ROLE_SUPER_ADMIN"));
 
-          userService.saveUser(new User(null, "John Travolta", "john", "1234", new ArrayList<>()));
-          userService.saveUser(new User(null, "Will Smith", "will", "1234", new ArrayList<>()));
-          userService.saveUser(new User(null, "Jim Carry", "jim", "1234", new ArrayList<>()));
-          userService.saveUser(new User(null, "Arnold Schwarzenegger", "arnold", "1234", new ArrayList<>()));
+            userService.saveUser(new User(null, "John Travolta", "john", "1234", new ArrayList<>(), new ArrayList<>()));
+            userService.saveUser(new User(null, "Will Smith", "will", "1234", new ArrayList<>(), new ArrayList<>()));
+            userService.saveUser(new User(null, "Jim Carry", "jim", "1234", new ArrayList<>(), new ArrayList<>()));
+            userService.saveUser(new User(null, "Arnold Schwarzenegger", "arnold", "1234", new ArrayList<>(), new ArrayList<>()));
 
-          userService.addRoleToUser("john", "ROLE_USER");
-          userService.addRoleToUser("john", "ROLE_MANAGER");
-          userService.addRoleToUser("will", "ROLE_MANAGER");
-          userService.addRoleToUser("jim", "ROLE_ADMIN");
-          userService.addRoleToUser("arnold", "ROLE_SUPER_ADMIN");
-          userService.addRoleToUser("arnold", "ROLE_ADMIN");
-          userService.addRoleToUser("arnold", "ROLE_USER");
-
+            userService.addRoleToUser("john", "ROLE_USER");
+            userService.addRoleToUser("john", "ROLE_MANAGER");
+            userService.addRoleToUser("will", "ROLE_MANAGER");
+            userService.addRoleToUser("jim", "ROLE_ADMIN");
+            userService.addRoleToUser("arnold", "ROLE_SUPER_ADMIN");
+            userService.addRoleToUser("arnold", "ROLE_ADMIN");
+            userService.addRoleToUser("arnold", "ROLE_USER");
         };
     }
 
